4e3fea44fe9924ebf6602196429856379c7ee8ea
sephson <disutj@gmail.com>
2021-10-21 16:49:14 +0100

    common problem solving patterns

--- algos.js
+++ algos.js
@@ -28,4 +28,93 @@ function charCount(str) {
   return freq;
 }
 
-console.log(charCount("Hello World"));
+//naive approach
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (i = 0; i < arr1.length; i++) {
+    if (arr2.indexOf(arr1[i] ** 2) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//naive approach
+function sameApp(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (i = 0; i < arr1.length; i++) {
+    if (arr2[i] !== arr1[i] ** 2) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//using frequency counter
+function sameFreq(arr1, arr2) {
+  let freqCounter1 = {};
+  let freqCounter2 = {};
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (x of arr1) {
+    if (!freqCounter1.hasOwnProperty(x)) {
+      freqCounter1[x] = 1;
+    } else {
+      freqCounter1[x]++;
+    }
+  }
+  for (x of arr2) {
+    if (!freqCounter2.hasOwnProperty(x)) {
+      freqCounter2[x] = 1;
+    } else {
+      freqCounter2[x]++;
+    }
+  }
+  for (key in freqCounter1) {
+    //check for keys
+    if (!(key ** 2 in freqCounter2)) {
+      return false;
+    }
+    //check for their values
+    if (freqCounter2[key ** 2] !== freqCounter1[key]) {
+      console.log(freqCounter2);
+      return false;
+    }
+  }
+  return true;
+}
+
+//anagrams
+function anagram1(str1, str2) {
+  freqCounter1 = {};
+  freqCounter2 = {};
+
+  for (char of str1) {
+    !freqCounter1.hasOwnProperty(char)
+      ? (freqCounter1[char] = 1)
+      : freqCounter1[char]++;
+  }
+
+  for (char of str2) {
+    !freqCounter2.hasOwnProperty(char)
+      ? (freqCounter2[char] = 1)
+      : freqCounter2[char]++;
+  }
+  for (key in freqCounter1) {
+    if (!(key in freqCounter2)) {
+      return false;
+    }
+    if (freqCounter1[key] !== freqCounter2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function sumZero(arr) {}
+console.log([-3, -2, -1, 0, 1, 2, 3]);
