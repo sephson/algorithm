a967a90962392adb54e7fa4a64b1837eeb5deb62
sephson <disutj@gmail.com>
2021-10-21 07:25:01 +0100

    freq count

--- algos.js
+++ algos.js
@@ -1,16 +1,31 @@
-function addUpTo(n){
-    let total = 0
-    for(i=0; i<=n; i++){
-        total += i
-    }
-    return total
+function addUpTo(n) {
+  let total = 0;
+  for (i = 0; i <= n; i++) {
+    total += i;
+  }
+  return total;
 }
 //O(n) runtime complexity, this is linear time complexity, as n increases, the time it takes to addup increases
 
-console.log(addUpTo(6))
-
-function addUpTo(n){
-    return n * (n + 1)/2
+function addUpTo(n) {
+  return (n * (n + 1)) / 2;
 }
 //O(1) runtime complexity
-console.log(addUpTo(6))
+
+function charCount(str) {
+  const lower = str.toLowerCase();
+  let freq = {};
+
+  for (char of lower) {
+    if (/a-z0-9/.test(char)) {
+      if (!freq.hasOwnProperty(char)) {
+        freq[char] = 1;
+      } else {
+        freq[char]++;
+      }
+    }
+  }
+  return freq;
+}
+
+console.log(charCount("Hello World"));
